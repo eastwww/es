cf172c69c9bf125fb6bc4fe68dad03c5a57cf8c8
wd <13926265364@163.com>
2020-03-26 16:25:31 +0800

    启动类注释

--- src/main/java/com/es/EsApplication.java
+++ src/main/java/com/es/EsApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EsApplication {
-
+	/**
+	 * 启动
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EsApplication.class, args);
 	}
